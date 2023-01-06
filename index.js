0d766c089ec300013d8f5e9e882d3488be832b5f
Kyrch <kyrchk5@gmail.com>
2023-01-05 21:45:35 -0300

    filters in front-end

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 
 let acceptLists = ['Rewatching', 'Completed ONA', 'Completed TV', 'Completed', 'Paused', 'Dropped'];
 let usersAnilist = ['Kyrch', 'Cesar', 'MiltonXerox'];
-let listAnimesPerUser = {};
 let listUsersPerAnime = {};
 
 main();
 
 async function main() {
     await getLists();
-    fs.writeFile("listUsersPerAnime.json", JSON.stringify(listUsersPerAnime), (err) => {})
+    fs.writeFile("listUsersPerAnime.json", JSON.stringify(listUsersPerAnime), (err) => { })
 }
 
 async function getLists() {
@@ -20,13 +19,16 @@ async function getLists() {
         let data = await anilist.lists.anime(userAnilist)
         let dataFiltersTypeList = data.filter(a => acceptLists.includes(a.name))
 
-        listAnimesPerUser[userAnilist] = []
         for (let typeJson of dataFiltersTypeList) {
-            entries = typeJson.entries.filter(a => a.media.episodes >= 10 && a.media.episodes <= 26 && a.media.duration >= 15);
 
-            entries.forEach(entry => {
+            typeJson.entries.forEach(entry => {
                 let romaji = entry.media.title.romaji;
-                Array.isArray(listUsersPerAnime[romaji]) ? listUsersPerAnime[romaji].push(userAnilist) : listUsersPerAnime[romaji] = [userAnilist];
+                listUsersPerAnime[romaji] = {};
+
+                Array.isArray(listUsersPerAnime[romaji]["users"]) ? listUsersPerAnime[romaji]["users"].push(userAnilist) : listUsersPerAnime[romaji]["users"] = [userAnilist];
+                listUsersPerAnime[romaji]["episodes"] = entry.media.episodes;
+                listUsersPerAnime[romaji]["duration"] = entry.media.duration;
+                listUsersPerAnime[romaji]["yearDate"] = entry.media.startDate.year;
             })
         }
     }
